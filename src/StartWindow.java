fafe077322f38d56a0e68056de584da6865eaa1a
lpiggott7 <34723853+lpiggott7@users.noreply.github.com>
2018-05-04 15:53:27 -0700

    added buttons, text, and functionality

--- src/StartWindow.java
+++ src/StartWindow.java
@@ -15,7 +15,10 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.*;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
-import javafx.scene.paint.ImagePattern; 
+import javafx.scene.paint.Color;
+import javafx.scene.paint.ImagePattern;
+import javafx.scene.text.Text;
+import javafx.scene.text.Font;
  
 public class StartWindow extends Application 
 {
@@ -37,13 +40,22 @@ public class StartWindow extends Application
     {	
 	    Scene home = new Scene(root, map_width, map_height);
     	Button startGame;
+    	Button howToPlay;
+    	Button settings;
+    	Text t = new Text(250, 100, "Mobs & Mayhem");
+    	t.setFill(Color.RED);
+    	t.setFont(Font.font("Verdana", 50));
     	startGame = new Button("Start Game");
-    	
+    	howToPlay = new Button("How To Play");
+    	settings = new Button("Settings");
+
+
+    	/*
     	Media m = new Media(getClass().getResource("assets/godfather_theme.mp3").toURI().toString());
 	    mp = new MediaPlayer(m);
 	    mp.setVolume(0.5);
 	    mp.play(); //Comment this out if you don't want music to play
-
+*/
 	    
     	
 		String url = "assets/start_screen.jpg";
@@ -55,7 +67,11 @@ public class StartWindow extends Application
 		
     	stage.setScene(home);
         stage.setTitle("Mobs & Mayhem");
-        root.getChildren().add(startGame);
+        howToPlay.setLayoutX(0);
+        howToPlay.setLayoutY(28);
+        settings.setLayoutX(0);
+        settings.setLayoutY(56);
+        root.getChildren().addAll(startGame, howToPlay, settings, t);
         stage.show();
         
         
@@ -64,11 +80,11 @@ public class StartWindow extends Application
     		stage.getScene().setRoot(game.getRootGroup());
     	});
     	
-    	
-
-     
+    	settings.setOnAction(e -> {
+    		SettingsWindow set = new SettingsWindow(home);
+    		stage.getScene().setRoot(set.getRootGroup());
+    	});   
     	
     }
    
-
 }
