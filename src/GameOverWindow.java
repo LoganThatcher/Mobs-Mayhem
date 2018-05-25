aaa8e3238e919b30de544605a2cbdb20c5fa33e7
lpiggott7 <34723853+lpiggott7@users.noreply.github.com>
2018-05-25 13:52:57 -0700

    made a scene rather than a stage

--- src/GameOverWindow.java
+++ src/GameOverWindow.java
@@ -14,7 +14,7 @@ import javafx.scene.text.Text;
 import javafx.scene.text.Font;
 
 
-public class GameOverWindow extends Application {
+public class GameOverWindow {
 	private static final int map_height = 600;
 	private static final int map_width = 900;
 	static final Group root = new Group();
@@ -23,29 +23,13 @@ public class GameOverWindow extends Application {
 	Image unmute_image, mute_image;
 	ImageView unmute_iv, mute_iv;
 
-	public static void main(String[] args) {
-		launch(args);
-
-	}
-	
-	@Override
-	public void start(Stage stage) throws Exception
+	public GameOverWindow (Scene go, Stage primaryStage)
     {	
-		/*
-	    Screen screen = Screen.getPrimary();
-	    Rectangle2D bounds = screen.getVisualBounds();
-	    stage.setX(bounds.getMinX());
-	    stage.setY(bounds.getMinY());
-	    stage.setWidth(bounds.getWidth());
-	    stage.setHeight(bounds.getHeight());
-	    */
-		
 		//Declarations
 		Font font = Font.loadFont(getClass().getResourceAsStream("assets/godfather.ttf"), 90);
 		Font font2 = Font.loadFont(getClass().getResourceAsStream("assets/godfather.ttf"), 150);
-		AudioClip click_player = new AudioClip(getClass().getResource("assets/click.mp3").toURI().toString());
-		AudioClip click2_player = new AudioClip(getClass().getResource("assets/click2.mp3").toURI().toString());
-	    Scene home = new Scene(root, map_width, map_height);
+		AudioClip click_player = new AudioClip(this.getClass().getResource("assets/click.mp3").toExternalForm());
+		AudioClip click2_player = new AudioClip(this.getClass().getResource("assets/click2.mp3").toExternalForm());
     	Button newGame, highScores, menu;
     	Text t;
     	Text G;
@@ -69,49 +53,20 @@ public class GameOverWindow extends Application {
     	E.setFont(font);
     	E.setFill(Color.WHITE);
     	
-    	/*F = new Text(225, 500, "Godfather");
-    	F.setFont(font);
-    	F.setFill(Color.RED); */
-    	
+  
     	//Buttons
     	newGame = new Button("New Game");
     	newGame.setFont(font);
     	highScores = new Button("High Scores");
     	highScores.setFont(font);
-    	menu = new Button("Return Menu");
+    	menu = new Button("Main Menu");
     	menu.setFont(font);
     	
-    	//instructions = new Button("Instructions");
-    	
-    	
-    	/*//Mute and Unmute images
-    	unmute_image = new Image(getClass().getResourceAsStream("assets/unmute.png"));
-    	unmute_iv = new ImageView(unmute_image);
-    	unmute_iv.setFitHeight(15);
-    	unmute_iv.setFitWidth(15);
-    	
-    	mute_image = new Image(getClass().getResourceAsStream("assets/mute.png"));
-    	mute_iv = new ImageView(mute_image);
-    	mute_iv.setFitHeight(15);
-    	mute_iv.setFitWidth(15);
-    	
-    	mute = new Button("",unmute_iv);*/
-    	
-
-    	//Music
-    	m = new Media(getClass().getResource("assets/godfather_theme.mp3").toURI().toString());
-	    mp = new MediaPlayer(m);
-	    mp.setVolume(0.35);
-	    mp.play(); 
-	    muted = false;
-
    	
 		img = new Image("assets/blackscreen.png");
 		
 		pattern = new ImagePattern(img);
-		home.setFill(pattern);
-    	stage.setScene(home);
-        stage.setTitle("Mobs & Mayhem");
+		go.setFill(pattern);
         
         newGame.setLayoutX(215);
         newGame.setLayoutY(450);
@@ -120,23 +75,10 @@ public class GameOverWindow extends Application {
         menu.setLayoutX(515);
         menu.setLayoutY(450);
         
-        //instructions.setLayoutX(450);
-        //instructions.setLayoutY(400);
-        
-        
         newGame.setPrefSize(150, 100);
         highScores.setPrefSize(150, 100);
         menu.setPrefSize(150, 100);
         
-        //instructions.setPrefSize(150, 100);
-        
-        
-        /*instructions.setStyle("-fx-background-color: transparent; -fx-text-fill: #ff0000; -fx-font-size: 20px");
-        instructions.setOnMouseEntered(e -> {
-        	instructions.setStyle("-fx-background-color: transparent; -fx-text-fill: #f8f8ff; -fx-font-size: 20px");
-        	click_player.play();
-        });
-        settings.setOnMouseExited(e -> settings.setStyle("-fx-background-color: transparent; -fx-text-fill: #ff0000; -fx-font-size: 20px"));*/
         
         newGame.setStyle("-fx-background-color: transparent; -fx-text-fill: #ff0000; -fx-font-size: 20px");
         newGame.setOnMouseEntered(e -> {
@@ -145,8 +87,7 @@ public class GameOverWindow extends Application {
         });
         newGame.setOnMouseExited(e -> newGame.setStyle("-fx-background-color: transparent; -fx-text-fill: #ff0000; -fx-font-size: 20px"));
         
-        
-        
+               
         highScores.setStyle("-fx-background-color: transparent; -fx-text-fill: #ff0000; -fx-font-size: 20px");
         highScores.setOnMouseEntered(e -> {
         	highScores.setStyle("-fx-background-color: transparent; -fx-text-fill: #f8f8ff; -fx-font-size: 20px");
@@ -154,8 +95,7 @@ public class GameOverWindow extends Application {
         });
         highScores.setOnMouseExited(e -> highScores.setStyle("-fx-background-color: transparent; -fx-text-fill: #ff0000; -fx-font-size: 20px"));
         
-        
-        
+         
         menu.setStyle("-fx-background-color: transparent; -fx-text-fill: #ff0000; -fx-font-size: 20px");
 		menu.setOnMouseEntered(e -> {
 			menu.setStyle("-fx-background-color: transparent; -fx-text-fill: #f8f8ff; -fx-font-size: 20px");
@@ -165,44 +105,33 @@ public class GameOverWindow extends Application {
 				        
 			
         root.getChildren().addAll(newGame, highScores, menu, t, G, E);
-        stage.show();
         
         
     	newGame.setOnAction(e -> {
     		
-    		LoadWindow load = new LoadWindow(home,stage);
-    		stage.getScene().setRoot(load.getRootGroup());
+    		LoadWindow load = new LoadWindow(go, primaryStage);
+    		primaryStage.getScene().setRoot(load.getRootGroup());
     		click2_player.play();
     		
     	});
     	
     	highScores.setOnAction(e -> {
-    		SettingsWindow set = new SettingsWindow(home, stage);
-    		stage.getScene().setRoot(set.getRootGroup());
+    		SettingsWindow set = new SettingsWindow(go, primaryStage);
+    		primaryStage.getScene().setRoot(set.getRootGroup());
     		click2_player.play();
     	});
     	
     	menu.setOnAction(e -> {
-    		MainMenuWindow main = new MainMenuWindow(home, stage);
-    		stage.getScene().setRoot(main.getRootGroup());
+    		MainMenuWindow main = new MainMenuWindow(go, primaryStage);
+    		primaryStage.getScene().setRoot(main.getRootGroup());
     		click2_player.play();
     	});
-
-    	
-
-    	
-    	/*mute.setOnAction(e -> {
-    		if(muted) {
-    			mp.play();
-    			mute.setGraphic(unmute_iv);
-    			muted = false;
-    		}else {
-    			mp.pause();
-    			mute.setGraphic(mute_iv);
-    			muted = true;
-    		}
-    	}); */
     	
     }
+	
+	public Group getRootGroup() {
+		return root;
+	}  
+
 
 }
