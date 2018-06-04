a9491f99efacf36428fb6e5d751a7f57528dbe63
Jason Fong <jfong27@calpoly.edu>
2018-06-03 23:31:09 -0700

    test stuff

--- src/test/GameWindowLoopTest.java
+++ src/test/GameWindowLoopTest.java
@@ -1,6 +1,9 @@
 package test;
 
 import static org.junit.Assert.*;
+import java.util.ArrayList;
+import javafx.scene.shape.Circle;
+
 
 import org.junit.Test;
 import logic.GameWindow;
@@ -8,8 +11,12 @@ import logic.GameWindow;
 public class GameWindowLoopTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testBirthCopsNone() {
+		ArrayList<Circle> expected = new ArrayList<Circle>();
+		ArrayList<Circle> actual = GameWindow.giveBirthToCops(0);
+		
+		assertEquals(expected, actual);
+		
 	}
 
 }
